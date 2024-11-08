2336094dc60d068f783610c6f3bc967e4afe5583
Niraviman-Singha <niraviman15-11689@diu.edu.bd>
2024-11-08 22:52:27 +0600

    My Voice Call App LoginActivity design part done.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.myvoicecallapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.myvoicecallapp"
@@ -44,4 +44,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+
+    implementation ("com.github.ZEGOCLOUD:zego_uikit_prebuilt_call_android:+")
 }
